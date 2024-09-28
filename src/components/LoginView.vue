54bac87828610a2daff043f060b6ddf60ee8c990
dgyim <yimdgkr@gmail.com>
2024-09-28 18:50:43 +0900

    - MembershipOptionView 기본 틀 작성 - vuetify 도입하여 login 창 개선

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -3,19 +3,6 @@
 </script>
 
 <template>
-  <div>
-    <div>
-      <label>아이디</label>
-      <input type="text">
-    </div>
-    <div>
-      <label>비밀번호</label>
-      <input type="text">
-    </div>
-    <div>
-      <button>로그인</button>
-    </div>
-  </div>
   <v-container>
     <v-row justify="center">
       <v-col sm="4">
