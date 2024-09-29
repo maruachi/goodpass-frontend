775c31c3b62cdebfb7648cae2156db2bf2162cce
dgyim <yimdgkr@gmail.com>
2024-09-29 18:40:26 +0900

    - 로그인 화면 구성

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -5,28 +5,38 @@
 <template>
   <v-container>
     <v-row justify="center">
-      <v-col sm="4">
-        <v-text-field
-            hint="아이디를 입력해주세요."
-            label="아이디"
-            type="input"
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row justify="center">
-      <v-col sm="4">
-        <v-text-field
-            hint="비밀번호를 입력해주세요."
-            label="비밀번호"
-            type="password"
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row justify="center">
-      <v-col sm="4" align="end">
-        <v-btn class="mr-2" color="primary">로그인</v-btn>
-        <v-btn color="primary">비밀번호 찾기</v-btn>
-      </v-col>
+      <v-card width="500">
+        <v-card-title align="center" class="text-h5">
+          Good Pass 로그인
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="form">
+            <v-text-field
+                label="아이디"
+                required
+            />
+            <v-text-field
+                label="비밀번호"
+                type="password"
+                required
+            />
+          </v-form>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn
+              text="로그인"
+              color="primary"
+              @click="login"
+          />
+
+          <v-spacer></v-spacer>
+
+          <v-btn
+              text="비밀번호 찾기"
+          />
+        </v-card-actions>
+      </v-card>
     </v-row>
   </v-container>
 </template>
